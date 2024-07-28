42a64678477ba21595507ff0322ee6d2561c012a
lukewiebe <mail@lukewiebe.com>
2024-07-28 17:13:34 -0500

    swap plugin and lua folders

--- lua/kontroll.lua
+++ lua/kontroll.lua
@@ -1,53 +1,11 @@
-vim.g.kontroll = {
-	colors = {
-		["Normal"] = "#FFFFFF",
-		["Visual"] = "#FF0000",
-		["Visual Line"] = "#AA0000",
-		["Visual Block"] = "#880000",
-		["Insert"] = "#00FF00",
-		["Command"] = "#0000FF",
-	},
-}
+local M = {}
 
-local autocmd_group = vim.api.nvim_create_augroup("Kontroll", { clear = true })
-
--- check if a keyboard is connected. If not, throw an error and make the user deal with it.
-local check_connected = function()
-	local obj = vim.system({ "kontroll", "restore-status-leds" }, { text = true }):wait()
-	if obj.code ~= 0 then
-		error(obj.stderr)
+M.setup = function(user_config)
+	default_config = vim.g.kontroll
+	if user_config ~= nil then
+		default_config = vim.tbl_deep_extend("force", default_config, user_config)
 	end
+	vim.g.kontroll = default_config
 end
 
--- change color on mode changed
-vim.api.nvim_create_autocmd({ "ModeChanged", "FocusGained" }, {
-	group = autocmd_group,
-	callback = function()
-		-- :h mode() for list of modes
-		local modes = {
-			["n"] = "Normal",
-			["v"] = "Visual",
-			["V"] = "Visual Line",
-			[""] = "Visual Block",
-			["s"] = "Select",
-			["i"] = "Insert",
-			["R"] = "Replace",
-			["c"] = "Command",
-			["r"] = "Normal",
-			["!"] = "Normal",
-			["l"] = "Normal",
-		}
-		local colors = vim.g.kontroll.colors
-		local current_mode = vim.fn.mode()
-		vim.system({ "kontroll", "set-rgb-all", "--color", colors[modes[current_mode]] }, { text = true }):wait()
-	end,
-})
-
--- release control of keyboard colors when neovim loses focus or exits
-vim.api.nvim_create_autocmd({ "VimLeavePre", "FocusLost" }, {
-	group = autocmd_group,
-		-- stylua: ignore
-		callback = function()
-			vim.system({ "kontroll", "restore-rgb-leds" }, { text = true }):wait()
-		end,
-})
+return M
